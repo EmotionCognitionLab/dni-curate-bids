9c7221359f621552c35cc74f9185b7d4d9f1cf73
Noah Mercer <noah_ten@yahoo.com>
2020-11-25 19:39:55 -0800

    Adds echo time setting

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import json
 import flywheel
 from flywheel_bids.curate_bids import main_with_args, curate_bids_dir
+import re
 
 if __name__ == '__main__':
 
@@ -35,3 +36,39 @@ if __name__ == '__main__':
     	session_only = False
 
     curate_bids_dir(fw, project_id, session_id, reset=reset, session_only=session_only,template_file=template_file)
+
+    # set the echotimes for any fieldmap acquisitions
+    mag1_pattern = re.compile("^.*_e1\\.nii\\.gz")
+    mag2_pattern = re.compile("^.*_e2\\.nii\\.gz")
+    phase_pattern = re.compile("^.*_e2_ph\\.nii\\.gz")
+
+    def set_echo_times(sess_id):
+        session = fw.get_session(sess_id)
+        acquisitions = session.acquisitions()
+        for acquisition in acquisitions:
+            # this may give incorrect results if an acquisition somehow has >1 mag1, mag2 or phase files
+            for f in acquisition.files:
+                if mag1_pattern.match(f.name):
+                    mag1 = f
+                elif mag2_pattern.match(f.name):
+                    mag2 = f
+                elif phase_pattern.match(f.name):
+                    phase = f
+
+        mag1_meta = fw.get_acquisition_file_info(mag1.parent.id,mag1.name)
+        mag2_meta = fw.get_acquisition_file_info(mag2.parent.id,mag2.name)
+        echotime1 = mag1_meta.info['EchoTime']
+        echotime2 = mag2_meta.info['EchoTime']
+        print(f"For {session.subject.label}/{session.label}, setting EchoTime1={echotime1:.5f}, EchoTime2={echotime2:.5f} in file {phase.name}.")
+        phase.update_info({"EchoTime1":echotime1,"EchoTime2":echotime2})
+
+    if session_only:
+        all_sessions = [session_id]
+    else:
+        project = fw.get_project(project_id)
+        search_result = fw.search({'return_type': 'session', 'structured_query': f"file.classification.Intent = 'Fieldmap' AND file.type = 'nifti' AND project.label = '{project.label}'"}, size=5000)
+        all_sessions = list(map(lambda item: item['session']['_id'], search_result))
+
+    for s_id in all_sessions:
+        set_echo_times(s_id)
+       
